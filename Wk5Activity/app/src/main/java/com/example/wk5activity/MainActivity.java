ce73ea02b7b290f8a4bc46f24fe40b8b3eecde35
Taint1266 <ilokadavid6@gmail.com>
2024-05-19 18:05:04 +0100

    Add files via upload

---
+++ Wk5Activity/app/src/main/java/com/example/wk5activity/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.wk5activity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText inputEmail,inputName;
+    private Button btn_save;
+    private TextView textViewEmail,textViewName;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputEmail=findViewById(R.id.inputEmail);
+        inputName=findViewById(R.id.inputName);
+
+        btn_save=findViewById(R.id.btn_save);
+
+
+
+
+
+
+    }
+}
